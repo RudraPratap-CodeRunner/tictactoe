3bb14da69caeebfaa22bf2819ad60683abd40f8a
RudraPratap-CodeRunner <rp.rudrapratapofficial@gmail.com>
2024-05-06 18:13:23 +0530

    tictac game code

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Tictactoe from './Components/Tictactoe/Tictactoe';
+
+function App() {
+  return (
+    <div>
+      <Tictactoe/>
+    </div>
+  );
+}
+
+export default App;
